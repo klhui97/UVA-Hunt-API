1f341ca6fa9d8f116bdad6fe67699025c98bc597
KL <huikldavid@gmail.com>
2018-10-02 23:28:58 +0800

    v1.1

--- Web/js/main.js
+++ Web/js/main.js
@@ -10,7 +10,13 @@ new Vue({
                     value: 'name'
                 },
                 {
-                    text: 'Total',
+                    text: 'CityU',
+                    align: 'center',
+                    sortable: false,
+                    value: 'cityu'
+                },
+                {
+                    text: 'All',
                     align: 'center',
                     sortable: false,
                     value: 'total'
@@ -29,15 +35,33 @@ new Vue({
                 sortable: false
             })
         },
-        tdContent(student, header) {
+        tdContent(student, header, isTips) {
             if (header.value == 'name') {
                 return '<div class="subheading text-xs-left">' + student.name + '</div>'
-            }else if (header.value == 'total') {
-                return '<div class="subheading text-xs-center">' + student.solvedQ.length + '</div>'
-            }else if (student[header.value]) {
+            } else if (header.value == 'cityu') {
+                return '<div class="subheading text-xs-center">' + student.solvedQ + '</div>'
+            } else if (header.value == 'total') {
+                return '<div class="subheading text-xs-center">' + student.totalSolvedQ + '</div>'
+            } else if (student[header.value] && student[header.value].success > 0) {
+                var data = student[header.value]
+                if (isTips) {
+                    return '<div>Total submission: ' + (data.fail + data.success) + '<br />Success: ' + data.success + '<br />Fail: ' + data.fail + '</div>'
+                }
                 return '<div class="blue white--text subheading text-xs-center">O</div>'
-            }else {
-                return '<div class="red white--text subheading text-xs-center">X</div>'
+            } else {
+                var data = student[header.value]
+                if (data) {
+                    if (isTips) {
+                        return '<div>Total submission: ' + data.fail + '<br />Success: 0<br />Fail: ' + data.fail + '</div>'
+                    }
+                    return '<div class="green white--text subheading text-xs-center">X</div>'
+                }else {
+                    if (isTips) {
+                        return '<div>No submission</div>'
+                    }
+                    return '<div class="red white--text subheading text-xs-center">X</div>'
+                }
+                
             }
         },
         getProblemString: function (pid) {
@@ -62,22 +86,46 @@ new Vue({
             $.each(json, function (index, item) {
                 $.getJSON('https://uhunt.onlinejudge.org/api/subs-user-last/' + item + '/10000', json => {
                     dict = {}
-                    solvedQ = []
                     dict['name'] = json.name
+                    solvedQ = 0
+                    totalSolvedQ = 0
+                    temp = {}
                     json.subs.forEach(s => {
-                        if (s[6] != -1) {
-                            dict[s[1]] = 'O'
+                        if (main.questionList[s[1]] && !dict[s[1]]) {
+                            if (s[6] != -1) {
+                                dict[s[1]] = {
+                                    fail: 0,
+                                    success: 1
+                                }
+                                solvedQ++
+                            } else {
+                                dict[s[1]] = {
+                                    fail: 1,
+                                    success: 0
+                                }
+                            }
+                        } else if (main.questionList[s[1]] && dict[s[1]]) {
+                            if (s[6] != -1) {
+                                if (dict[s[1]].success == 0) {
+                                    solvedQ++
+                                }
+                                dict[s[1]].success++
+                            } else {
+                                dict[s[1]].fail++
+                            }
                         }
-                    });
-                    for (var k in dict) {
-                        if (k != 'name') {
-                            solvedQ.push(k)
+                        
+                        if (s[6] != -1 && !temp[s[1]]) {
+                            totalSolvedQ++
+                            temp[s[1]] = 'O'
                         }
-                    }
+                    })
+                    delete temp
                     dict['solvedQ'] = solvedQ
+                    dict['totalSolvedQ'] = totalSolvedQ
                     main.studentData.push(dict)
-                })
+                });
             })
         })
-    },
+    }
 })
